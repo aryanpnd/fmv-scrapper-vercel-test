651fce1df133e8a8431463a7c665e10fc49dab9c
aryanpnd <captainaryan9870@gmail.com>
2025-01-19 23:13:31 +0530

    initiated

--- server.js
+++ server.js
@@ -5,11 +5,20 @@ import { umsLogin } from './loginScrapper.js';
 const app = express();
 app.use(bodyParser.json());
 
+app.get('/', (req, res) => {
+  res.send('Welcome to UMS Login API Test');
+});
 // define the first route
 app.post('/login', async (req, res) => {
   const result = await umsLogin(req.body);
   res.json(result);
 });
+
+app.get('/*', (req, res) => {
+  res.redirect('/');
+});
+
+
 
 // start the server listening for requests
 app.listen(3000, () => console.log("Server is running..."));
